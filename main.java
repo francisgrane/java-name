1587da1c71a752609194abcab765cf439b0260ba
francisgrane <120190258+francisgrane@users.noreply.github.com>
2024-03-13 17:15:05 +0530

    Update main.java

--- main.java
+++ main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         port(8080); // Set port
 
-        get("/", (req, res) -> "Hello, Your Name!"); // Display your name
+        get("/", (req, res) -> "Hello, This is Test Application"); // Display your name
     }
 }
