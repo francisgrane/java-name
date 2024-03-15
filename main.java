932c9ac77fe0476440defad63db03b680ab99b39
francisgrane <120190258+francisgrane@users.noreply.github.com>
2024-03-15 11:11:28 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,5 @@
-import static spark.Spark.*;
-
 public class Main {
     public static void main(String[] args) {
-        port(8080); // Set port
-
-        get("/", (req, res) -> "Hello, This is Test Application"); // Display your name
+        System.out.println("Hello, Francis Grane!");
     }
 }
